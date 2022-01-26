f9494bb459be6ba7ae67e7ba29b4fe3c2664d241
JSYoo <dbwhdtjr0457@khu.ac.kr>
2022-01-26 23:55:17 +0900

    20220126

---
+++ CodeTest/python/1546.py
@@ -0,0 +1,19 @@
+from audioop import avg
+
+
+num = int(input())
+data = list(map(int, input().split()))
+
+maxNum = max(data)
+
+for i in range(num):
+    data[i] = (data[i] / maxNum) * 100
+
+avgNum = 0
+
+for i in range(num):
+    avgNum += data[i]
+
+avgNum /= num
+
+print(avgNum)
