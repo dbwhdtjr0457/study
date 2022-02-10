b1cfcac0e3cf52de436de0045f16859728915ada
JSYoo <dbwhdtjr0457@khu.ac.kr>
2022-02-10 23:07:40 +0900

    20220210

--- CodeTest/python/2580.py
+++ CodeTest/python/2580.py
@@ -1,19 +1,49 @@
-n = int(input())
+import sys
 
-numList = [0] * 4
 
-numList[1] = 1
-numList[2] = 2
-numList[3] = 4
+import sys
+input = sys.stdin.readline
+
+
+data = []
+
+for _ in range(9):
+    data.append(list(map(int, input().split())))
+
+zeroList = []
+for i in range(9):
+    for j in range(9):
+        if data[i][j] == 0:
+            zeroList.append((i, j))
 
 
 def solution(n):
-    if n <= 3:
-        return numList[n]
-    else:
-        return solution(n - 1) + solution(n - 2) + solution(n - 3)
+    if n == len(zeroList):
+        for row in range(9):
+            print(*data[row])
+        return True
+
+    x = zeroList[n][0]
+    y = zeroList[n][1]
+
+    for num in range(1, 10):
+        able = True
+        for i in range(9):
+            if data[x][i] == num or data[i][y] == num:
+                able = False
+
+        if able:
+            for i in range(x // 3 * 3, x // 3 * 3 + 3):
+                for j in range(y // 3 * 3, y // 3 * 3 + 3):
+                    if data[i][j] == num:
+                        able = False
+
+        if able:
+            data[x][y] = num
+            if solution(n + 1) == True:
+                return True
+
+    data[x][y] = 0
 
 
-for _ in range(n):
-    num = int(input())
-    print(solution(num))
+solution(0)
