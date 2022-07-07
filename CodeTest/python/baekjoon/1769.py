ff34afb3efa0e50974473febaf85fb523b1a0386
dbwhdtjr0457 <dbwhdtjr0457@khu.ac.kr>
2022-07-07 23:10:56 +0900

    20220707

--- CodeTest/python/baekjoon/1769.py
+++ CodeTest/python/baekjoon/1769.py
@@ -1,3 +1,4 @@
+"""
 n = int(input())
 
 count = 0
@@ -9,3 +10,22 @@ if n % 3:
     print("NO")
 else:
     print("YES")
+    """
+
+def check(num_list, cnt):
+    if len(num_list) == 1:
+        return int(num_list[0]), cnt
+    else:
+        summary = list(str(sum(list(map(int, num_list)))))
+        return check(summary, cnt + 1)
+
+
+N = list(input())
+cnt = 0
+
+N, cnt = check(N, cnt)
+print(cnt)
+if N % 3 == 0:
+    print("YES")
+else:
+    print("NO")
