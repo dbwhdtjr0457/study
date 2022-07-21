33a685ce8ba4d4b93abcda27ad2e591700e8ca1f
dbwhdtjr0457 <dbwhdtjr0457@khu.ac.kr>
2022-07-22 01:55:21 +0900

    20220722

--- javascript/Deep-Dive/index.js
+++ javascript/Deep-Dive/index.js
@@ -1,1 +1,7 @@
-console.log(true == 1)
+for (var i = 0; i < 6; i++) {
+    setTimeout(() => console.log(i), 100);
+  }
+  
+  for (let i = 0; i < 6; i++) {
+    setTimeout(() => console.log(i), 100);
+  }
